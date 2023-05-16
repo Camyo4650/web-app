7865e538811e9f6f09ef50a340db153ba4f5978f
Camyo4650 <campilchard@gmail.com>
2023-05-15 20:58:43 -0500

    Fixing app

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import NavBar from './components/Navbar';
+import NavBar from './components/NavBar';
 import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
 import { Container } from 'react-bootstrap';
